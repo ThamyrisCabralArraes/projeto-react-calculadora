6dc1d538cd9f12a3da53d78b5acc01cb6d53380a
thamyris <thamyriscabral@hotmail.com>
2023-01-17 10:24:34 -0300

    realizado display

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -1,26 +1,85 @@
 import React, { Component } from 'react';
 import './Calculator.css';
 import Button from '../componets/Button';
+import Display from '../componets/Display';
 
 class Calculadora extends Component {
+  clearMemory = () => {
+    console.log('limpar');
+  };
+
+  setOperation = (operation) => {
+    console.log('setOperat');
+  };
+
+  addDigit = (n) => {
+    console.log('addDig');
+  };
+
   render() {
     return (
       <div className='calculadora'>
-        <Button label='AC' />
-        <Button label='/' />
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='*' />
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
-        <Button label='0' />
+        <Display value={100} />
+        <Button
+          label='AC'
+          click={this.clearMemory}
+        />
+        <Button
+          label='/'
+          click={this.setOperation}
+        />
+        <Button
+          label='7'
+          click={this.addDigit}
+        />
+        <Button
+          label='8'
+          click={this.addDigit}
+        />
+        <Button
+          label='9'
+          click={this.addDigit}
+        />
+        <Button
+          label='*'
+          click={this.setOperation}
+        />
+        <Button
+          label='4'
+          click={this.addDigit}
+        />
+        <Button
+          label='5'
+          click={this.addDigit}
+        />
+        <Button
+          label='6'
+          click={this.addDigit}
+        />
+        <Button
+          label='-'
+          click={this.setOperation}
+        />
+        <Button
+          label='1'
+          click={this.addDigit}
+        />
+        <Button
+          label='2'
+          click={this.addDigit}
+        />
+        <Button
+          label='3'
+          click={this.addDigit}
+        />
+        <Button
+          label='+'
+          click={this.setOperation}
+        />
+        <Button
+          label='0'
+          click={this.addDigit}
+        />
         <Button label='.' />
         <Button label='=' />
       </div>
--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
-  return <button className='button'>{props.label}</button>;
+  let classes = 'button';
+  classes += props.double ? 'double' : '';
+  classes += props.triple ? 'triple' : '';
+  classes += props.operation ? 'operation' : '';
+
+  return (
+    <button
+      onClick={(e) => props.click && props.click(console.log(props.label))}
+      className={classes}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
