357c015ea77e357f02632790653f05958cfd838c
thamyris <thamyriscabral@hotmail.com>
2023-01-17 09:26:01 -0300

    feito os botoes da calculadora

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Calculadora from './calculator/Calculator';
+
+function App() {
+  return (
+    <>
+      <h1>Calculadora</h1>
+      <Calculadora />
+    </>
+  );
+}
+
+export default App;
