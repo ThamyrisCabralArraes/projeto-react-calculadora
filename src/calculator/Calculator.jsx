9d927b27413445cd36b9e0f38d1667f276796707
thamyris <thamyriscabral@hotmail.com>
2023-02-01 10:39:28 -0300

    finalizando calculadora

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Calculator.css';
 import Button from '../componets/Button';
 import Display from '../componets/Display';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
 
 const inicialState = {
   displayValue: '0',
@@ -19,7 +20,27 @@ class Calculadora extends Component {
   };
 
   setOperation = (operation) => {
-    console.log('setOperat');
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const currentOperation = this.state.operation;
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   };
 
   addDigit = (n) => {
@@ -31,6 +52,14 @@ class Calculadora extends Component {
     const currentValue = clearDisplay ? '' : this.state.displayValue;
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== '.') {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
+    }
   };
 
   render() {
